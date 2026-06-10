d97a213599965b7e6eb2f6ac89d2d3f63b3c23b6
Lance <lanceandreiducante@gmail.com>
2026-06-10 10:13:34 +0800

    created navbar and hero section

---
+++ script.js
@@ -0,0 +1,9 @@
+function openSideBar() {
+  const sideBar = document.querySelector('.side-bar');
+  sideBar.style.display = 'flex';
+}
+
+function closeSideBar() {
+  const sideBar = document.querySelector('.side-bar');
+  sideBar.style.display = 'none';
+}
