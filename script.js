70ac3f8c47932516a401ca59f2da446d94f5821c
Lance <lanceandreiducante@gmail.com>
2026-06-11 11:25:29 +0800

    added navbar background blur glass effect

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function closeSideBar() {
   const sideBar = document.querySelector('.side-bar');
   sideBar.style.display = 'none';
 }
+
+const navbar = document.querySelector('.nav-bar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
